0d7be177ea496dc89639a52b22e3d74c3a438bd2
huangjian <huangjian@ft.com>
2021-07-26 18:02:40 +0800

    m

---
+++ RedisClient/RedisClient/RedisClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  RedisClientApp.swift
+//  RedisClient
+//
+//  Created by huangjian on 7/26/21.
+//
+
+import SwiftUI
+
+@main
+struct RedisClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
